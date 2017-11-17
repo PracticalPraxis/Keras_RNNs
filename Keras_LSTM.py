006ec2f4dab7c92ba7d52702fbc8bd74cc4a162a
PracticalPraxis <htradingm@gmail.com>
2017-11-17 13:32:00 +0000

    Update Keras_LSTM.py

--- Keras_LSTM.py
+++ Keras_LSTM.py
@@ -1,6 +1,6 @@
 #In this model we'll be evaluating the IMDB datset using just one RNN, the Long Short Term Memory (LSTM) layer
 #This model achieves a great validation accuracy of 89%, but is also significantly more 
-#expensieve (approx 10 minutes on a GTX 980m)
+#expensieve than many alternatives (approx 10 minutes on a GTX 980m)
 from keras.layers import SimpleRNN
 from keras.models import Sequential
 from keras.layers import Embedding, SimpleRNN
@@ -15,7 +15,8 @@ max_features = 10000
 maxlen = 500
 batch_size = 32
 
-#Preparing our data print('Loading data...')
+#Preparing our data 
+print('Loading data...')
 (input_train, y_train), (input_test, y_test) = imdb.load_data(num_words=max_features)
 print(len(input_train), 'train sequences')
 print(len(input_test), 'test sequences')
@@ -29,9 +30,10 @@ print('input_test shape:', input_test.shape)
 model = Sequential()
 model.add(Embedding(max_features, 32))
 #The LSTM is a modified RNN layer that implements an additional information channel that backpropagates
-#additional data from previous loops used in the model
-#Additionally, this carried over data is transformed according to certain weights that works to 
+#data from previous loops used in the model
+#Additionally, this carried over data is transformed according to certain weights that work to 
 #optimise the data e.g. by forgetting irrelevant information
+#For a full mathematical explanation see: https://www.cs.ox.ac.uk/people/nando.defreitas/machinelearning/lecture11.pdf
 model.add(LSTM(32))
 model.add(Dense(1, activation='sigmoid'))
 
@@ -39,9 +41,11 @@ model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
 history = model.fit(input_train, y_train,
 					epochs=10,
 					batch_size=128,
-					#New Parameter here, validation split means that the given fraction of the total data will be used as the validation data
+					#New Parameter here, validation split means that the given fraction of the total 
+		    			#data will be used as the validation data
 					validation_split=0.2)
 
+#End by plotting our data
 acc = history.history['acc']
 val_acc = history.history['val_acc']
 loss = history.history['loss']
